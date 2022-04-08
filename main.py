aadf0f242e43e6cbe720062ae98704ceba0c1f13
Povladislav <54409358+Povladislav@users.noreply.github.com>
2022-04-08 10:32:11 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,54 @@
+                #task1
+# a = 5
+# print(a)
+                #task2
+# a = input('Введите число с клавиатуры: ')
+# print(a)
+                #task3
+# a = (input('Введите число с клавиатуры: '))
+# c = int(a)
+# print(c)
+                #task4 & task5
+
+# a = int(input('Введите значение первой переменной: '))
+# b = int(input('Введите значение второй переменной: '))
+#
+# dif = a - b
+# sum = a + b
+# quot = a / b
+# mult = a * b
+# ost = a % b
+#
+# print(dif,sum,quot,mult,ost)
+                #task6
+
+# a = int(input("Введите первое число: "))
+# b = int(input("Введите второе число: "))
+# c = int(input("Введите третье число: "))
+# sum = a + b + c
+# print(f'Сумма: {sum}')
+
+                #task7
+# kat1 = int(input("Длина первого катета: "))
+# kat2 = int(input("Длина второго катета: "))
+# s = 0.5 * kat1 * kat2
+# print(f"Площадь равна: {s}")
+
+                #task8
+# name = input('Введите ваше имя: ')
+# print(f"Hello {name}!!!")
+
+                #task9
+# count_sc = int(input('Введите количество школьников: '))
+# apples = int(input('Введите количество яблок: '))
+# apples_in_busket = count_sc % apples
+# apples_to_everyone = int(count_sc / apples)
+# print(f"В корзине: {apples_in_busket}\nКаждому по {apples_to_everyone} яблок")
+
+
+
+
+
+
+
+
